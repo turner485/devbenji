8e1c9b5a59d4c96e2b0ffb9aceeb2c4bb952b3bc
Ben TUrner <turner485@hotmail.co.uk>
2024-07-22 15:35:19 +0100

    filter by push date

--- src/app/components/Repos.js
+++ src/app/components/Repos.js
@@ -14,10 +14,12 @@ const Repos = () => {
                 const username = 'turner485'; 
                 const repos = await getUserRepositories(username);
       
-                const sortedRepos = repos.sort((start_of_time, most_recent) => new Date(most_recent.created_at) - new Date(start_of_time.created_at));
+                const sortedRepos = repos.sort((repoA, repoB) => new Date(repoB.pushed_at) - new Date(repoA.pushed_at));
+
 
                 setRepositories(sortedRepos);
                 setLoading(false);
+                console.log(repos);
             } catch (error) {
                 console.error('Error fetching repositories:', error);
             }
@@ -40,9 +42,10 @@ const Repos = () => {
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        <li className='rounded bg-slate-700 hover:bg-[#7395c0] ease-out duration-300 mb-6 px-6 pt-4 pb-4 min-h-48 relative'>
+                        <li className='flex flex-col rounded bg-slate-700 hover:bg-[#7395c0] ease-out duration-300 mb-6 px-6 pt-4 pb-4 min-h-72 relative'>
                             <p className='text-xl font-bold capitalize'>{repo.name.replace(/[-_]/g, ' ')}</p>
                             <p className='text-base'>{repo.description}</p>
+                            <p className='created-at text-sm'>Created at: {repo.pushed_at.replace(/[TZ]/g, ' ')}</p>
                             <div className='fa-wrapper fa-icon'>
                                 <FontAwesomeIcon icon={faGithub} className="text-4xl text-white absolute right-1 bottom-1" />
                             </div>
