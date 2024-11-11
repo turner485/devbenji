06fc0f44ddc944245915d8f7a80b0727e4d595ee
Ben TUrner <turner485@hotmail.co.uk>
2024-11-11 13:46:06 +0000

    hide url

--- src/app/components/Freelance.js
+++ src/app/components/Freelance.js
@@ -6,7 +6,7 @@ const Freelance = () => {
     return (
         <div className="freelance">
             <li className='flex flex-col rounded bg-slate-700 hover:bg-[#7395c0] ease-out duration-300 mb-6 px-6 pt-4 pb-4 min-h-72 relative'>
-                <p className='text-xl font-bold'>edinburghtextiles.co.uk</p>
+                <p className='text-xl font-bold'>WooCommerce Build</p>
                 <p className='text-base'>
                     E-commerce website built using WordPress with WooCommerce for a friend&rsquo;s business. The site features a user-friendly interface, secure payment options, and a responsive design to ensure a seamless shopping experience across all devices. The project included custom theme development, integration of various plugins for enhanced functionality, and optimization for SEO to drive organic traffic.<br /><br />
                 </p>
