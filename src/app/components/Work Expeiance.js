c3b954c5f45f363f99cc379dd9bddb0c5961cd3d
Ben TUrner <turner485@hotmail.co.uk>
2024-11-18 17:25:04 +0000

    update cv

--- src/app/components/Work Expeiance.js
+++ src/app/components/Work Expeiance.js
@@ -1,69 +1,216 @@
 
 const WorkExperience = () => {
     return (
-        <div className='work-experience overflow-y-auto '>
-            <div className="block">
-                <h4>[Technical Skills]</h4>
-                <p>Python3 | Django | JavaScript | Vue | React | Node | Jquery | PHP | MySQL & MongoDB | API | HTML5 & CSS3 | SASS/LESS | Git | BASH | Grunt, Gulp & Webpack | Magento | Wordpress | Responsive design/development</p><br />
-                <h4>[Other Notable Skills]</h4>
-                <p>AWS (EC2, S3, Lambda, Amplify, RDS, DynamoDB, API gateway) | Docker | Selenium | PSD-tools | pillow | Hybris | Impex | Jinja | TailwindCSS | Material UI | ImageMagick | Adobe Creative Cloud Suite | LATEX| Linux | Windows | Mac IOS</p><br />
-            </div>
-            <div className="work-ex-block">
-                <h3>[Developer - Joules]</h3>
-                <p>- Led cross-departmental projects in E-commerce, Marketing, ECRM, and digital design, driving innovative solutions to fruition.<br /></p>
-                <p>- Proficiently developed fully functional front-end pages using cutting-edge web technologies to meet critical business demands.<br /></p>
-                <p>- Managed and analyzed extensive datasets, such as SKU codes, extracting valuable insights.<br /></p>
-                <p>- Employed Python and Impex to create automation scripts, conduct optimization tests, and streamline workflow processes, Mentored and trained junior team members, contributing to their professional growth.<br /></p>
-                <p>- Built and configured an image optimization bash script using ImageMagick.<br /></p>
-                <p>- Developed and maintained several Python automation scripts for the BAU program, increasing build efficiency.<br /></p>
-                <p> - Combined Python and Impex to create an image block Impex for efficiency.<br /></p>
-                <p>- Implemented switching from image to picture tags in all BAU for optimization.<br /></p>
-                <p> - Added webp format support to the bash image script for optimization using HTML picture tags.<br /></p>
-                <p> - Improved grunt workflow, saving time on load time.< br /><br /></p>
-                
-                <h4 className="underline">Projects</h4>
-                <b>#1 Automating the site refresh | Python Automation</b>
-                <p>Led the development of an automated solution for refreshing the project, presented a prototype to key stakeholders, and successfully implemented the Python refresh automation, reducing build time by 120 hours (5 days).</p><br />
-                <b>#2 Profanity Filter Script | Python csv & argsparse</b>
-                <p>Developed a profanity filter script to systematically remove inappropriate content from large datasets, ensuring data integrity and professionalism.</p><br />
-                <b>#3 Site scraping script | Python/BeautifulSoup4/smtplib</b><br />
-                <p>Created site scraping scripts to gather targeted online data, established an SMTP email list, and implemented a cron job to send daily emails with collected data, providing valuable insights into stock levels.</p><br />
-                <b>#4 Image optimization scripting | Bash & ImageMagick</b>
-                <p>Developed a bash script using ImageMagick for image compression, significantly reducing page load times and enhancing user experiences. This optimization saved the business considerable time, achieving a 1.5s page load time.</p><br />
-                <b>#5 Impex Python population | Python & Impex</b>
-                <p>Created a Python-based solution to streamline the image export process from Photoshop and facilitate seamless importation into the CMS system, saving 2-3 hours of manual uploading.</p><br />
-            </div>
-            <div className="work-ex-block">
-                <h3>[Junior Web Developer - Joules]</h3>
-                <p>- Enhanced e-commerce sites and excelled in emaildevelopment during initial two years at Joules.<br /></p> 
-                <p>- Collaborated closely with the in-house design team to ensure accurate and precise project delivery.<br /></p> 
-                <p>- Created a Python script for automation, significantly improving efficiency and reducing build time for BAU projects.<br /></p> 
-                <p>- Assumed the role of head of the email program after the manager’s departure.<br /></p> 
-                <p>- Established a standardized modular library using Python automation, enabling efficient email creation while adhering to brand guidelines.<br /></p>
-                <br />
-            </div>
-            <div className="work-ex-block">
-                <h3>[Junior Front-End Web Developer - Wholesale LED lights]</h3>
-                <p>- Lead front-end in-house developer, building and maintaining Magento e-commerce websites.<br /></p> 
-                <p>- Created responsive websites from scratch to drive traffic toward new business ideas.<br /></p> 
-                <p>- Adhered to best practices for version control via GitHub, ensuring proper staging and production environments.<br /></p> 
-                <p>- Followed strict coding standards, including commenting, indentation, and logic.<br /></p> 
-                <p>- Improved website design and usability.<br /></p> 
-                <p>- Optimized legacy code.<br /></p> 
-                <br />
-            </div>
-            <div className="work-ex-block">
-                <h3>[Junior Front-End Web Developer - Auction Marketer]</h3>
-                <p>- Built and maintained online bidding software for a diverse client base<br /></p> 
-                <p>- Adhered to version control best practices on GitHub.<br /></p> 
-                <p>- Managed workload organization and scheduling using Asana and Harvest.<br /></p> 
-                <p>- Ensured seamless integration of work into client builds and proper version control documentation.<br /></p> 
-                <p>- Implemented rigorous testing, including cross-platform and cross-browser testing.<br /></p> 
-                <p>- Developed strategies for streamlined product roll-outs to maximize profitability.<br /></p>
-                <br /><br />
-            </div>
-        </div>
-    )
+		<div className="work-experience overflow-y-auto ">
+			<div className="block">
+				<h4>[Technical Skills]</h4>
+				<p>
+					Python | JavaScript | HTML | CSS | SASS/LESS | REST API |
+					Git | BASH | Grunt | Gulp | Node | JSON | Wordpress |
+					Magento | Responsive Design | UX/UI | SQL | MySQL | Postgres
+					| ImageMagick | Material UI | Tailwin | Bootstrap | Adobe
+					Photoshop | Markdown | LaTex | Windows | Linux | MacOS
+				</p>
+				<br />
+				<h4>[Other Notable Skills]</h4>
+				<p>
+					Django | React | Next | Vue | Angular | AWS | Docker |
+					Webpack | Selenium | Postman | MongoDB | Firebase
+				</p>
+				<br />
+			</div>
+			<div className="work-ex-block">
+				<h3>[Developer - Joules]</h3>
+				<p>
+					- Directed cross-departmental projects in E-commerce,
+					Marketing, ECRM, and digital design, delivering innovative
+					solutions that enhanced user engagement and increased
+					cross-functional collaboration.
+					<br />
+				</p>
+				<p>
+					- Automated and optimized the entire weekly sprint workflow,
+					utilizing Python and third-party tools to streamline
+					processes, improve operational efficiency, and reduce the
+					chance of human error by up to 90% across the e-commerce
+					website.
+					<br />
+				</p>
+				<p>
+					- Built responsive front-end web pages optimized for best
+					web practices, including accessibility and security, using
+					core front-end technologies: HTML, CSS/SASS, JavaScript.
+					<br />
+				</p>
+				<p>
+					- Applied web scraping scripts using Python, requests, and
+					BeautifulSoup4 to deliver business-critical data to
+					stakeholders, such as site stock information.
+					<br />
+				</p>
+				<p>
+					- Mentored and developed junior team members, fostering
+					growth and enhancing overall team performance.
+					<br />
+				</p>
+				<p>
+					- Streamlined image imports/exports to our e-commerce
+					platform (SAP Hybris) by automating renaming and sorting
+					processes with Python
+					<br />
+				</p>
+				<p>
+					- Consistently adhered to coding best practices for both
+					Python and web development, utilized version control with
+					Git, and actively participated in code reviews by submitting
+					code for review and regularly reviewing peers’ code
+					<br />
+				</p>
+				<p>
+					- Developed an internal developer wiki with Python, Django,
+					and React to centralize team knowledge and streamline
+					collaboration. Proposed and led this project, implementing a
+					Django backend and React frontend API for efficient
+					documentation and knowledge sharing.
+					<br />
+				</p>
+
+				<h4 className="underline">Projects</h4>
+				<b>#1 Automating the site refresh | Python Automation</b>
+				<p>
+					Led the development of a Python automation solution that
+					reduced project build time by 120 hours (5 days),
+					significantly improving delivery speed.
+				</p>
+				<br />
+				<b>#2 Profanity Filter Script | Python csv & argsparse</b>
+				<p>
+					Developed a script to systematically remove inappropriate
+					content from large datasets, ensuring data integrity and
+					professional standards.
+				</p>
+				<br />
+				<b>#3 Site scraping script | Python/BeautifulSoup4/smtplib</b>
+				<br />
+				<p>
+					Created a site scraping script to gather targeted online
+					data, implemented a cron job for daily email updates,
+					providing valuable insights into stock levels.
+				</p>
+				<br />
+				<b>#4 Image optimization scripting | Bash & ImageMagick</b>
+				<p>
+					Developed a bash script for image compression, significantly
+					reducing page load times and achieving a 1.5-second load
+					time, enhancing user experience.
+				</p>
+				<br />
+				<b>#5 Impex Python population | Python & Impex</b>
+				<p>
+					Streamlined the image export/import process from Photoshop
+					to CMS, saving 2-3 hours of manual work per project
+				</p>
+				<br />
+			</div>
+			<div className="work-ex-block">
+				<h3>[Junior Web Developer - Joules]</h3>
+				<p>
+					- Enhanced e-commerce sites and excelled in emaildevelopment
+					during initial two years at Joules.
+					<br />
+				</p>
+				<p>
+					- Collaborated closely with the in-house design team to
+					ensure accurate and precise project delivery.
+					<br />
+				</p>
+				<p>
+					- Created a Python script for automation, significantly
+					improving efficiency and reducing build time for BAU
+					projects.
+					<br />
+				</p>
+				<p>
+					- Assumed the role of head of the email program after the
+					manager’s departure.
+					<br />
+				</p>
+				<p>
+					- Established a standardized modular library using Python
+					automation, enabling efficient email creation while adhering
+					to brand guidelines.
+					<br />
+				</p>
+				<br />
+			</div>
+			<div className="work-ex-block">
+				<h3>[Junior Front-End Web Developer - Wholesale LED lights]</h3>
+				<p>
+					- Lead front-end in-house developer, building and
+					maintaining Magento e-commerce websites.
+					<br />
+				</p>
+				<p>
+					- Created responsive websites from scratch to drive traffic
+					toward new business ideas.
+					<br />
+				</p>
+				<p>
+					- Adhered to best practices for version control via GitHub,
+					ensuring proper staging and production environments.
+					<br />
+				</p>
+				<p>
+					- Followed strict coding standards, including commenting,
+					indentation, and logic.
+					<br />
+				</p>
+				<p>
+					- Improved website design and usability.
+					<br />
+				</p>
+				<p>
+					- Optimized legacy code.
+					<br />
+				</p>
+				<br />
+			</div>
+			<div className="work-ex-block">
+				<h3>[Junior Front-End Web Developer - Auction Marketer]</h3>
+				<p>
+					- Built and maintained online bidding software for a diverse
+					client base
+					<br />
+				</p>
+				<p>
+					- Adhered to version control best practices on GitHub.
+					<br />
+				</p>
+				<p>
+					- Managed workload organization and scheduling using Asana
+					and Harvest.
+					<br />
+				</p>
+				<p>
+					- Ensured seamless integration of work into client builds
+					and proper version control documentation.
+					<br />
+				</p>
+				<p>
+					- Implemented rigorous testing, including cross-platform and
+					cross-browser testing.
+					<br />
+				</p>
+				<p>
+					- Developed strategies for streamlined product roll-outs to
+					maximize profitability.
+					<br />
+				</p>
+				<br />
+				<br />
+			</div>
+		</div>
+	);
 }
 
 export default WorkExperience;
